fdaaba43d13823d14d2c0c77e461f5c92ef5f737
smile-vi <895179246@qq.com>
2020-04-02 11:40:58 +0800

    111

---
+++ shop-service-impl/shop-service-sso/src/main/java/com/service/sso/SsoApplication.java
@@ -0,0 +1,15 @@
+package com.service.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 马运动
+ * @date 2020/4/2 11:40
+ */
+@SpringBootApplication
+public class SsoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SsoApplication.class,args);
+    }
+}
