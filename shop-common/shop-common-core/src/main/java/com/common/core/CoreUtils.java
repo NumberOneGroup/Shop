ff59b59a75d84cb4b4bae343b048ec22b989ea05
smile-vi <895179246@qq.com>
2020-04-02 11:45:21 +0800

    解决解决问题

---
+++ shop-common/shop-common-core/src/main/java/com/common/core/CoreUtils.java
@@ -0,0 +1,8 @@
+package com.common.core;
+
+/**
+ * @author 马运动
+ * @date 2020/4/2 11:44
+ */
+public class CoreUtils {
+}
---
+++ shop-service-api/shop-service-api-sso/src/main/java/com/service/api/TestService.java
@@ -0,0 +1,8 @@
+package com.service.api;
+
+/**
+ * @author 马运动
+ * @date 2020/4/2 11:44
+ */
+public class TestService {
+}
